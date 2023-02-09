93660832523eb2b4ea6c62e1cdc308f1fd07a46d
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-02-09 12:42:38 -0300

    Check for factorial of 0 to be equal 1

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,6 +2,11 @@ require 'rspec'
 require_relative '../solver'
 
 describe Solver do
-
+  describe "#factorial" do
+    it 'returns number 1 to factorial of 0' do
+      factorial = Solver.new
+      expect(factorial.factorial(0)).to eql 1
+    end
+  end
 end
 
